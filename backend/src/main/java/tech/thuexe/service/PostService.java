77da66bc5338de16f6fe00082d54291891eaf799
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2022-04-13 19:35:17 +0700

    Revert "add API get image"
    This reverts commit 6417a3d2a25a614f5c594d59ca761b2278062ab0.

--- backend/src/main/java/tech/thuexe/service/PostService.java
+++ backend/src/main/java/tech/thuexe/service/PostService.java
@@ -2,7 +2,6 @@ package tech.thuexe.service;
 
 import tech.thuexe.DTO.post.PostReadDTO;
 import tech.thuexe.DTO.post.PostWriteDTO;
-import tech.thuexe.entity.ImageEntity;
 import tech.thuexe.entity.PostEntity;
 
 import java.util.List;
@@ -10,5 +9,4 @@ import java.util.List;
 public interface PostService {
     PostReadDTO save(PostWriteDTO postWriteDTO);
     List<PostReadDTO> getPosts();
-    ImageEntity getImage(int id);
 }
