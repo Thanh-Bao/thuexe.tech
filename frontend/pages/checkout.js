00f887d153de16f323c405a34563c06c9d8c7d4f
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2022-07-19 08:54:32 +0700

    add example checkout

--- frontend/pages/checkout.js
+++ frontend/pages/checkout.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Head from '@/layout/web/head';
+import WebLayout from '@/layout/web';
+import { makeStyles } from '@mui/styles';
+// import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({
+    helloWorld: {
+        backgroundColor: 'red'
+    },
+    helloWorld2: {
+        color: 'blue'
+    }
+}));
 
 const Checkout = () => {
+    const classes = useStyles();
     return (
         <>
-            <div>Hello World</div>
+            <Head
+                title="checkout"
+            />
+            <WebLayout>
+                <div
+                    className={`${classes.helloWorld} ${classes.helloWorld2}`}
+                >
+                    Hello World
+                </div>
+            </WebLayout>
+
         </>
     )
 }
